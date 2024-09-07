768d0f73ad78358eaadfeaf2fcf6f335c77db939
Julio Sanic <julio.sanic.gt.256@gmail.com>
2024-09-07 14:42:55 -0600

    header page mobile

--- src/components/header/PageHeader.tsx
+++ src/components/header/PageHeader.tsx
@@ -6,7 +6,7 @@ interface PageHeaderProps {
 const PageHeader: React.FC<PageHeaderProps> = ({ title, className }) => {
   return (
     <div
-      className={`${className} bg-no-repeat bg-center bg-cover h-[30vh] flex justify-start items-center p-4`}
+      className={`${className} bg-no-repeat bg-center bg-cover h-[15vh] sm:h-[30vh] flex justify-start items-center p-4`}
     >
       <h1 className="text-white text-3xl font-bold tracking-wider">{title}</h1>
     </div>
